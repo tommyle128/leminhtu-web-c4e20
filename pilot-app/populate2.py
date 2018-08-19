50d9889c8bb483724253864a07b74249f63eb7c6
tommyle128 <minhtu.le128@gmail.com>
2018-08-19 17:12:52 +0700

    initial commit

--- pilot-app/populate2.py
+++ pilot-app/populate2.py
@@ -1,18 +1,23 @@
 from models.customer import Customer
 import mlab
 from faker import Faker
-from random import randint, choice
+from random import choice
 
 mlab.connect()
 
 fake = Faker()
 for i in range (50):
-    print("Saving to Customer (Collection),",i+1,"...")
+    print("Saving to Customer (Collection)",i+1,"...")
 
     profile = fake.profile()
+    if profile['sex'] == 'M':
+        g = 1
+    else:
+        g = 0
+
     new_customer = Customer(
         name =  profile['name'],
-        gender = randint(0, 1),
+        gender = g,
         email = profile['mail'],
         phone = fake.phone_number(),
         job = profile['job'],
